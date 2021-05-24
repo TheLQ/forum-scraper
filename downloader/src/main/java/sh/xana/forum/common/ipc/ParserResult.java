fac566d6c15a415cf6cb3aa91f13a982a68c8a34
Leon Blakey <code@leonblakey.com>
2021-05-24 17:09:44 -0400

    match parser js

--- downloader/src/main/java/sh/xana/forum/common/ipc/ParserResult.java
+++ downloader/src/main/java/sh/xana/forum/common/ipc/ParserResult.java
@@ -2,6 +2,6 @@ package sh.xana.forum.common.ipc;
 
 import sh.xana.forum.common.dbutil.DatabaseStorage;
 
-public record ParserResult(ParserEntry[] entries) {
+public record ParserResult(DatabaseStorage.PageType type, ParserEntry[] entries) {
   public record ParserEntry(String name, String url, DatabaseStorage.PageType type) {}
 }
