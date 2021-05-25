d533359873c9861c4f2e65f4df84ed3e8c75a1ad
Leon Blakey <code@leonblakey.com>
2021-05-24 23:37:11 -0400

    pom tweaks for jpackage

--- downloader/src/main/java/sh/xana/forum/server/dbutil/JooqGenerator.java
+++ downloader/src/main/java/sh/xana/forum/server/dbutil/JooqGenerator.java
@@ -1,24 +1,24 @@
-package sh.xana.forum.server.dbutil;
-
-import org.jooq.codegen.GenerationTool;
-import org.jooq.meta.jaxb.*;
-
-/** Run jOOq CodeGen. Automatically handles removing old files too */
-public class JooqGenerator {
-  public static void main(String[] args) throws Exception {
-    Configuration config =
-        new Configuration()
-            .withJdbc(new Jdbc().withDriver("org.sqlite.JDBC").withUrl("jdbc:sqlite:sample.db"))
-            .withGenerator(
-                new Generator()
-                    .withDatabase(
-                        new Database()
-                            .withName("org.jooq.meta.sqlite.SQLiteDatabase")
-                            .withIncludes(".*"))
-                    .withTarget(
-                        new Target()
-                            .withPackageName("sh.xana.forum.server.db")
-                            .withDirectory("src/main/java")));
-    GenerationTool.generate(config);
-  }
-}
+//package sh.xana.forum.server.dbutil;
+//
+//import org.jooq.codegen.GenerationTool;
+//import org.jooq.meta.jaxb.*;
+//
+///** Run jOOq CodeGen. Automatically handles removing old files too */
+//public class JooqGenerator {
+//  public static void main(String[] args) throws Exception {
+//    Configuration config =
+//        new Configuration()
+//            .withJdbc(new Jdbc().withDriver("org.sqlite.JDBC").withUrl("jdbc:sqlite:sample.db"))
+//            .withGenerator(
+//                new Generator()
+//                    .withDatabase(
+//                        new Database()
+//                            .withName("org.jooq.meta.sqlite.SQLiteDatabase")
+//                            .withIncludes(".*"))
+//                    .withTarget(
+//                        new Target()
+//                            .withPackageName("sh.xana.forum.server.db")
+//                            .withDirectory("src/main/java")));
+//    GenerationTool.generate(config);
+//  }
+//}
