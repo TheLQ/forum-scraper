300978affe7664ecd01309f00bdddc854cae7e79
Leon Blakey <code@leonblakey.com>
2021-06-20 16:47:39 -0400

    parser: added formatter

--- parser/src/utils.ts
+++ parser/src/utils.ts
@@ -1,88 +1,93 @@
-import {Cheerio, CheerioAPI, Element} from "cheerio";
+import {Cheerio, CheerioAPI, Element} from 'cheerio';
 
 export interface SourcePage {
-    rawHtml: string,
-    $: CheerioAPI,
-    baseUrl: string,
+  rawHtml: string;
+  $: CheerioAPI;
+  baseUrl: string;
 }
 
 export interface Result {
-    loginRequired: boolean,
-    pageType: PageType,
-    forumType: ForumType | null,
-    subpages: Subpage[],
+  loginRequired: boolean;
+  pageType: PageType;
+  forumType: ForumType | null;
+  subpages: Subpage[];
 }
 
 interface Subpage {
-    name: string,
-    url: string,
-    pageType: PageType,
+  name: string;
+  url: string;
+  pageType: PageType;
 }
 
 // Copied from downloader/src/main/java/sh/xana/forum/server/dbutil/DatabaseStorage.java
 export enum PageType {
-    ForumList = "ForumList",
-    TopicPage = "TopicPage",
-    Unknown = "Unknown",
+  ForumList = 'ForumList',
+  TopicPage = 'TopicPage',
+  Unknown = 'Unknown',
 }
 
 export enum ForumType {
-    ForkBoard = "ForkBoard",
-    vBulletin = "vBulletin",
-    phpBB = "phpBB",
-    XenForo = "XenForo",
-    SMF = "SMF",
+  ForkBoard = 'ForkBoard',
+  vBulletin = 'vBulletin',
+  phpBB = 'phpBB',
+  XenForo = 'XenForo',
+  SMF = 'SMF',
 }
 
 export function assertNotNull(value: string | undefined | null): string {
-    if (value == null || value == undefined) {
-        throw new Error("Value is null")
-    }
-    return value
+  if (value == null || value == undefined) {
+    throw new Error('Value is null');
+  }
+  return value;
 }
 
 export function assertNotBlank(valueRaw: string | undefined | null): string {
-    const value = assertNotNull(valueRaw).trim()
-    if (value == "") {
-        throw new Error("value is blank")
-    }
-    return value;
+  const value = assertNotNull(valueRaw).trim();
+  if (value == '') {
+    throw new Error('value is blank');
+  }
+  return value;
 }
 
 export function getBaseUrl(sourcePage: SourcePage): string {
-    const baseQuery = sourcePage.$("head > base")
-    if (baseQuery.length != 0) {
-        return assertNotBlank(baseQuery.last().attr('href'));
-    } else {
-        // fallback to baseUrl
-        return sourcePage.baseUrl
-    }
+  const baseQuery = sourcePage.$('head > base');
+  if (baseQuery.length != 0) {
+    return assertNotBlank(baseQuery.last().attr('href'));
+  } else {
+    // fallback to baseUrl
+    return sourcePage.baseUrl;
+  }
 }
 
-export function makeUrlWithBase(baseUrl: string, mainUrl: string | undefined | null): string {
-    mainUrl = assertNotBlank(mainUrl)
+export function makeUrlWithBase(
+  baseUrl: string,
+  mainUrl: string | undefined | null
+): string {
+  mainUrl = assertNotBlank(mainUrl);
 
-    if (mainUrl.startsWith("http:") || mainUrl.startsWith("https:")) {
-        return mainUrl
-    }
-    if (mainUrl.startsWith("/")) {
-        mainUrl = mainUrl.substr(1)
-    }
-    if (!baseUrl.endsWith("/")) {
-        baseUrl = baseUrl + "/"
-    }
-    return baseUrl + mainUrl
+  if (mainUrl.startsWith('http:') || mainUrl.startsWith('https:')) {
+    return mainUrl;
+  }
+  if (mainUrl.startsWith('/')) {
+    mainUrl = mainUrl.substr(1);
+  }
+  if (!baseUrl.endsWith('/')) {
+    baseUrl = baseUrl + '/';
+  }
+  return baseUrl + mainUrl;
 }
 
 export function getFirstMatch(element: Cheerio<Element>, errorMessage: string) {
-    if (element.length == 0) {
-        throw new Error("didn't find anything for " + errorMessage)
-    } else if (element.length > 1) {
-        throw new Error("found too many " +element.length + " for " + errorMessage)
-    }
-    return element.first();
+  if (element.length == 0) {
+    throw new Error("didn't find anything for " + errorMessage);
+  } else if (element.length > 1) {
+    throw new Error(
+      'found too many ' + element.length + ' for ' + errorMessage
+    );
+  }
+  return element.first();
 }
 
 export function anchorIsNavNotLink(elem: Element) {
-    return elem.attribs.name != undefined && elem.attribs.href == undefined;
+  return elem.attribs.name != undefined && elem.attribs.href == undefined;
 }
--- parser/src/web.ts
+++ parser/src/web.ts
@@ -1,44 +1,54 @@
-import { readResponseFile } from "./parser"
-import uWS from "uWebSockets.js"
+import uWS from 'uWebSockets.js';
+import {readResponseFile} from './forums/AbstractForum';
 
 export async function mainWeb(args: string[]): Promise<number> {
-    if (args.length != 1) {
-        console.log("node parser.js server [pathToFileCache]")
-        return 1
-    }
-    const fileCachePath = args[0]
-    console.log("running webserver with fileCachePath " + fileCachePath)
+  if (args.length != 1) {
+    console.log('node parser.js server [pathToFileCache]');
+    return 1;
+  }
+  const fileCachePath = args[0];
+  console.log('running webserver with fileCachePath ' + fileCachePath);
 
-    const port = 3000
+  const port = 3000;
 
-    uWS.App({
-    }).get("/:id", async (res, req) => {
-        res.onAborted(() => {
-            res.aborted = true;
-        });
+  uWS
+    .App({})
+    .get('/:id', async (res, req) => {
+      res.onAborted(() => {
+        res.aborted = true;
+      });
 
-        // url format is /[uuid]?[baseUrl]
-        let r = await getResponse(req.getParameter(0), fileCachePath, req.getQuery())
+      // url format is /[uuid]?[baseUrl]
+      const r = await getResponse(
+        req.getParameter(0),
+        fileCachePath,
+        req.getQuery()
+      );
 
-        if (!res.aborted) {
-            res.end(r);
-        }
-    }).listen(port, (token: any) => {
-        if (token) {
-            console.log('Listening to port ' + port);
-        } else {
-            console.log('Failed to listen to port ' + port);
-        }
+      if (!res.aborted) {
+        res.end(r);
+      }
+    })
+    .listen(port, (token: any) => {
+      if (token) {
+        console.log('Listening to port ' + port);
+      } else {
+        console.log('Failed to listen to port ' + port);
+      }
     });
 
-    return 0
+  return 0;
 }
 
-async function getResponse(id: string, fileCachePath: string, baseUrl: string): Promise<string> {
-    try {
-        const response = await readResponseFile(fileCachePath, id, baseUrl)
-        return JSON.stringify(response)
-    } catch (e) {
-        return "" + e;
-    }
+async function getResponse(
+  id: string,
+  fileCachePath: string,
+  baseUrl: string
+): Promise<string> {
+  try {
+    const response = await readResponseFile(fileCachePath, id, baseUrl);
+    return JSON.stringify(response);
+  } catch (e) {
+    return '' + e;
+  }
 }
