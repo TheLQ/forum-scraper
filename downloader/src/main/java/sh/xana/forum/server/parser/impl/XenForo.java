57dbda51238322ed4a61ebe1a376031a1a5a172d
Leon Blakey <code@leonblakey.com>
2021-06-30 21:05:57 -0400

    Parser: Handle original XenForo detection ids

--- downloader/src/main/java/sh/xana/forum/server/parser/impl/XenForo.java
+++ downloader/src/main/java/sh/xana/forum/server/parser/impl/XenForo.java
@@ -2,6 +2,7 @@ package sh.xana.forum.server.parser.impl;
 
 import java.util.Collection;
 import javax.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.nodes.Element;
 import sh.xana.forum.common.ipc.ParserResult;
 import sh.xana.forum.server.dbutil.ForumType;
@@ -11,7 +12,7 @@ import sh.xana.forum.server.parser.SourcePage;
 public class XenForo implements AbstractForum {
   @Override
   public @Nullable ForumType detectForumType(String rawHtml) {
-    if (rawHtml.contains("<html id=\"XF\"")) {
+    if (rawHtml.contains("<html id=\"XF\"") || rawHtml.contains("<html id=\"XenForo\"")) {
       return ForumType.XenForo;
     } else {
       return null;
@@ -24,22 +25,22 @@ public class XenForo implements AbstractForum {
   }
 
   @Override
-  public Collection<Element> getPageLinks(SourcePage sourcePage) {
+  public @NotNull Collection<Element> getPageLinks(SourcePage sourcePage) {
     return sourcePage.doc().select(".pageNav-page a, .PageNav a");
   }
 
   @Override
-  public Collection<Element> getPostElements(SourcePage sourcePage) {
+  public @NotNull Collection<Element> getPostElements(SourcePage sourcePage) {
     return sourcePage.doc().select(".messageContent, .message-content");
   }
 
   @Override
-  public Collection<Element> getSubforumAnchors(SourcePage sourcePage) {
+  public @NotNull Collection<Element> getSubforumAnchors(SourcePage sourcePage) {
     return sourcePage.doc().select(".node-title a, .nodeTitle a");
   }
 
   @Override
-  public Collection<Element> getTopicAnchors(SourcePage sourcePage) {
+  public @NotNull Collection<Element> getTopicAnchors(SourcePage sourcePage) {
     //return sourcePage.doc().select("a[qid=\"thread-item-title\"]");
     return sourcePage.doc().select(".title a, .structItem-title a");
   }
