4b9cd8ab4c2bc1ff7fec20dc6b08dddeab8d4dce
Leon Blakey <code@leonblakey.com>
2021-07-06 19:14:28 -0400

    SQL: Minor function improvements

--- downloader/database.mariadb.sql
+++ downloader/database.mariadb.sql
@@ -84,33 +84,37 @@ CREATE TABLE `Sites` (
 -- Dump completed on 2021-06-19 22:51:05
 
 -- source https://stackoverflow.com/a/58015720/342518
+-- and https://mariadb.com/kb/en/guiduuid-performance/
+
+DROP FUNCTION IF EXISTS `BIN_TO_UUID`;
+DROP FUNCTION IF EXISTS `UUID_TO_BIN`;
 
 DELIMITER $$
 
-CREATE FUNCTION BIN_TO_UUID(b BINARY(16), f BOOLEAN)
+CREATE FUNCTION BIN_TO_UUID(b BINARY(16))
 RETURNS CHAR(36)
-DETERMINISTIC
+LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
 BEGIN
    DECLARE hexStr CHAR(32);
    SET hexStr = HEX(b);
    RETURN LOWER(CONCAT(
-        IF(f,SUBSTR(hexStr, 9, 8),SUBSTR(hexStr, 1, 8)), '-',
-        IF(f,SUBSTR(hexStr, 5, 4),SUBSTR(hexStr, 9, 4)), '-',
-        IF(f,SUBSTR(hexStr, 1, 4),SUBSTR(hexStr, 13, 4)), '-',
+        SUBSTR(hexStr, 1, 8), '-',
+        SUBSTR(hexStr, 9, 4), '-',
+        SUBSTR(hexStr, 13, 4), '-',
         SUBSTR(hexStr, 17, 4), '-',
         SUBSTR(hexStr, 21)
     ));
 END$$
 
 
-CREATE FUNCTION UUID_TO_BIN(uuid CHAR(36), f BOOLEAN)
+CREATE FUNCTION UUID_TO_BIN(uuid CHAR(36))
 RETURNS BINARY(16)
-DETERMINISTIC
+LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
 BEGIN
   RETURN UNHEX(CONCAT(
-  IF(f,SUBSTRING(uuid, 15, 4),SUBSTRING(uuid, 1, 8)),
+  SUBSTRING(uuid, 1, 8),
   SUBSTRING(uuid, 10, 4),
-  IF(f,SUBSTRING(uuid, 1, 8),SUBSTRING(uuid, 15, 4)),
+  SUBSTRING(uuid, 15, 4),
   SUBSTRING(uuid, 20, 4),
   SUBSTRING(uuid, 25))
   );
