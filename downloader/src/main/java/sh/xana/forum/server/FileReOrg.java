6c8520113ec9992f58253374b2a7cfd833bccceb
Leon Blakey <code@leonblakey.com>
2021-09-26 12:24:31 -0400

    FileReOrg: Added post cleanup

--- downloader/src/main/java/sh/xana/forum/server/FileReOrg.java
+++ downloader/src/main/java/sh/xana/forum/server/FileReOrg.java
@@ -1,17 +1,23 @@
 package sh.xana.forum.server;
 
+import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/** Re-organize filecache directory to implement and/or verify a/b/abcd... paths */
 public class FileReOrg {
   private static final Logger log = LoggerFactory.getLogger(FileReOrg.class);
 
   public static void main(String[] args) throws Exception {
     ServerConfig config = new ServerConfig();
+    Path fileCacheDir = Path.of(config.get(config.ARG_FILE_CACHE));
 
+    // pregenerate hex character list
     String[] hexChars = new String[6 + 10];
     int hexIndex = 0;
     for (char i = 'a'; i <= 'f'; i++) {
@@ -21,36 +27,60 @@ public class FileReOrg {
       hexChars[hexIndex++] = "" + i;
     }
 
-    Path fileCacheDir = Path.of(config.get(config.ARG_FILE_CACHE));
-    for (String outer : hexChars) {
-      for (String inner : hexChars) {
-        try {
-          Path dir = fileCacheDir.resolve("" + outer).resolve("" + inner);
+    // create directories
+    for (String level1 : hexChars) {
+      for (String level2 : hexChars) {
+        Path dir = fileCacheDir.resolve("" + level1).resolve("" + level2);
+        if (!Files.isDirectory(dir)) {
           log.info("mkdir {}", dir);
           Files.createDirectory(dir);
-        } catch (FileAlreadyExistsException e) {
-          // ignore...
         }
       }
     }
 
-    for (String outer : hexChars) {
-      log.info("dir {}", outer);
-      Path outerDir = fileCacheDir.resolve("" + outer);
-      Files.list(outerDir)
-          .forEach(
-              e -> {
-                if (Files.isDirectory(e)) {
-                  return;
-                }
-                String name = e.getFileName().toString();
-                Path newPath = outerDir.resolve("" + name.charAt(1)).resolve(name);
+    AtomicInteger counter = new AtomicInteger();
+    AtomicInteger movedCounter = new AtomicInteger();
+    Files.walk(fileCacheDir)
+        .parallel()
+        .forEach(
+            currentPath -> {
+              if (Files.isDirectory(currentPath)) {
+                return;
+              }
+
+              int idx = counter.incrementAndGet();
+              if (idx % 10000 == 0) {
+                log.info("Processed {} moved {}", idx, movedCounter);
+              }
+
+              String name = currentPath.getFileName().toString();
+              Path newPath =
+                  fileCacheDir
+                      .resolve("" + name.charAt(0))
+                      .resolve("" + name.charAt(1))
+                      .resolve(name);
+              if (!currentPath.equals(newPath)) {
+                movedCounter.incrementAndGet();
                 try {
-                  Files.move(e, newPath);
-                } catch (Exception ex) {
-                  throw new RuntimeException(ex);
+                  Files.move(currentPath, newPath);
+                } catch (FileAlreadyExistsException ex) {
+                  try {
+                    if (Files.getLastModifiedTime(currentPath)
+                            .compareTo(Files.getLastModifiedTime(newPath))
+                        > 0) {
+                      log.debug("overwriting older duplicate {}", currentPath);
+                      Files.move(currentPath, newPath, StandardCopyOption.REPLACE_EXISTING);
+                    } else {
+                      log.debug("deleting duplicate {}", currentPath);
+                      Files.delete(currentPath);
+                    }
+                  } catch (IOException ex2) {
+                    throw new RuntimeException("nested fail", ex2);
+                  }
+                } catch (IOException ex) {
+                  throw new RuntimeException("io", ex);
                 }
-              });
-    }
+              }
+            });
   }
 }
