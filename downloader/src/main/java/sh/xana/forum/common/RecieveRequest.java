8002d8ed4d40189b941c95181aa856c2f64bc9b8
Leon Blakey <code@leonblakey.com>
2021-10-20 20:01:52 -0400

    SqsManager: Port to AWS SDK v2...

--- downloader/src/main/java/sh/xana/forum/common/RecieveRequest.java
+++ downloader/src/main/java/sh/xana/forum/common/RecieveRequest.java
@@ -1,5 +1,5 @@
 package sh.xana.forum.common;
 
-import com.amazonaws.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.Message;
 
 public record RecieveRequest<T>(Message message, T obj) {}
--- downloader/src/main/java/sh/xana/forum/common/SqsManager.java
+++ downloader/src/main/java/sh/xana/forum/common/SqsManager.java
@@ -2,26 +2,6 @@ package sh.xana.forum.common;
 
 import com.amazon.sqs.javamessaging.AmazonSQSExtendedClient;
 import com.amazon.sqs.javamessaging.ExtendedClientConfiguration;
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.AmazonWebServiceRequest;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.retry.PredefinedRetryPolicies;
-import com.amazonaws.retry.PredefinedRetryPolicies.SDKDefaultRetryCondition;
-import com.amazonaws.retry.RetryPolicy;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.CreateQueueRequest;
-import com.amazonaws.services.sqs.model.DeleteMessageBatchRequestEntry;
-import com.amazonaws.services.sqs.model.ListQueuesResult;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.QueueDoesNotExistException;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import com.amazonaws.services.sqs.model.ReceiveMessageResult;
-import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
 import java.io.Closeable;
 import java.net.URI;
 import java.util.ArrayList;
@@ -30,38 +10,57 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sh.xana.forum.common.ipc.IScraperRequest;
 import sh.xana.forum.common.ipc.ScraperDownload;
 import sh.xana.forum.common.ipc.ScraperUpload;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.DeleteQueueRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueAttributesRequest;
+import software.amazon.awssdk.services.sqs.model.ListQueuesRequest;
+import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
+import software.amazon.awssdk.services.sqs.model.QueueDoesNotExistException;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 public class SqsManager implements Closeable {
   private static final Logger log = LoggerFactory.getLogger(SqsManager.class);
 
   public static final int QUEUE_SIZE = 10;
   public static final String QUEUE_DOWNLOAD_PREFIX = "forumDownload-";
-  private final AmazonSQSExtendedClient sqsClient;
+  private final SqsClient sqsClient;
   private final URI uploadQueueUrl;
 
   private final List<URI> cachedDownloadQueueUrls = new ArrayList<>();
 
   public SqsManager(CommonConfig config) {
     if (!config.isDebugMode()) {
-      ClientConfiguration clientConfiguration =
-          new ClientConfiguration()
-              .withRetryPolicy(
-                  new RetryPolicy(
-                      new MyRetryCondition(),
-                      PredefinedRetryPolicies.DEFAULT_BACKOFF_STRATEGY,
+      ClientOverrideConfiguration clientConfiguration =
+          ClientOverrideConfiguration.builder()
+              .retryPolicy(
+                  RetryPolicy.builder()
+                      // RetryCondition.defaultRetryCondition() is... a thing
+                      // .retryCondition(new MyRetryCondition())
                       // aggressive retry forever due to network errors
-                      50,
-                      true));
-      AmazonS3 s3 =
-          AmazonS3ClientBuilder.standard().withClientConfiguration(clientConfiguration).build();
-      AmazonSQS sqsClientBackend =
-          AmazonSQSClientBuilder.standard().withClientConfiguration(clientConfiguration).build();
+                      .numRetries(50)
+                      .build())
+              .build();
+
+      S3Client s3 = S3Client.builder().overrideConfiguration(clientConfiguration).build();
+      SqsClient sqsClientBackend =
+          SqsClient.builder().overrideConfiguration(clientConfiguration).build();
 
       ExtendedClientConfiguration extendedClientConfig =
           new ExtendedClientConfiguration()
@@ -86,9 +85,11 @@ public class SqsManager implements Closeable {
 
   public void updateDownloadQueueUrls() {
     synchronized (cachedDownloadQueueUrls) {
-      ListQueuesResult listQueuesResult = sqsClient.listQueues(QUEUE_DOWNLOAD_PREFIX);
+      ListQueuesResponse listQueuesResult =
+          sqsClient.listQueues(
+              ListQueuesRequest.builder().queueNamePrefix(QUEUE_DOWNLOAD_PREFIX).build());
       cachedDownloadQueueUrls.clear();
-      for (String queue : listQueuesResult.getQueueUrls()) {
+      for (String queue : listQueuesResult.queueUrls()) {
         cachedDownloadQueueUrls.add(Utils.toURI(queue));
       }
     }
@@ -103,9 +104,13 @@ public class SqsManager implements Closeable {
         isize =
             Integer.parseInt(
                 sqsClient
-                    .getQueueAttributes(queueUrl.toString(), List.of("ApproximateNumberOfMessages"))
-                    .getAttributes()
-                    .get("ApproximateNumberOfMessages"));
+                    .getQueueAttributes(
+                        GetQueueAttributesRequest.builder()
+                            .queueUrl(queueUrl.toString())
+                            .attributeNames(QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES)
+                            .build())
+                    .attributes()
+                    .get(QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES));
       } catch (QueueDoesNotExistException e) {
         // silently catch, deleted queues can appear in subsequent list
         log.warn("Failed to get queue " + queueUrl + " might of been deleted", e);
@@ -125,13 +130,13 @@ public class SqsManager implements Closeable {
 
   public String newDownloadQueue(String domain) {
     domain = SqsManager.getQueueNameSafe(domain);
-    final CreateQueueRequest createQueueRequest =
-        new CreateQueueRequest().withQueueName(QUEUE_DOWNLOAD_PREFIX + domain);
-    return sqsClient.createQueue(createQueueRequest).getQueueUrl();
+    return sqsClient
+        .createQueue(CreateQueueRequest.builder().queueName(QUEUE_DOWNLOAD_PREFIX + domain).build())
+        .queueUrl();
   }
 
   public void deleteQueue(URI uri) {
-    sqsClient.deleteQueue(uri.toString());
+    sqsClient.deleteQueue(DeleteQueueRequest.builder().queueUrl(uri.toString()).build());
   }
 
   public void sendDownloadRequests(URI queue, List<ScraperDownload> entries) {
@@ -159,24 +164,27 @@ public class SqsManager implements Closeable {
       if (singlate) {
         for (IScraperRequest send : sends) {
           String json = Utils.jsonMapper.writeValueAsString(send);
-          sqsClient.sendMessage(queueUrl.toString(), json);
+          sqsClient.sendMessage(
+              SendMessageRequest.builder().queueUrl(queueUrl.toString()).messageBody(json).build());
         }
       } else {
         List<SendMessageBatchRequestEntry> messages = new ArrayList<>();
         for (IScraperRequest entry : sends) {
           String json = Utils.jsonMapper.writeValueAsString(entry);
           messages.add(
-              new SendMessageBatchRequestEntry()
-                  .withMessageBody(json)
+              SendMessageBatchRequestEntry.builder()
+                  .messageBody(json)
                   // TODO: Need to force unique id's because getMessageId is duplicated?
-                  .withId(UUID.randomUUID().toString()));
-          if (messages.size() == 10) {
-            sqsClient.sendMessageBatch(queueUrl.toString(), messages);
+                  .id(UUID.randomUUID().toString())
+                  .build());
+          if (messages.size() == 10 || entry == sends.get(sends.size() - 1)) {
+            sqsClient.sendMessageBatch(
+                SendMessageBatchRequest.builder()
+                    .entries(messages)
+                    .queueUrl(queueUrl.toString())
+                    .build());
             messages.clear();
           }
-        }
-        if (!messages.isEmpty()) {
-          sqsClient.sendMessageBatch(queueUrl.toString(), messages);
         }
       }
     } catch (Exception e) {
@@ -186,46 +194,21 @@ public class SqsManager implements Closeable {
 
   private <T> List<RecieveRequest<T>> receive(URI queueUrl, Class<T> clazz) {
     try {
-      Exception receiveException = null;
-      ReceiveMessageResult receiveMessageResult = null;
-      for (int i = 0; i < 5; i++) {
-        try {
-          // given numbers are maximum supported
-          ReceiveMessageRequest receiveMessageRequest =
-              new ReceiveMessageRequest()
-                  .withQueueUrl(queueUrl.toString())
-                  .withMaxNumberOfMessages(10)
-                  .withWaitTimeSeconds(20);
-
-          receiveMessageResult = sqsClient.receiveMessage(receiveMessageRequest);
-          break;
-        } catch (AmazonServiceException e) {
-          receiveException = e;
-          if (e.getCause() instanceof AmazonS3Exception) {
-            log.warn(
-                "S3 failure for queue "
-                    + queueUrl
-                    + ", retry "
-                    + i
-                    + " - "
-                    + e.getMessage()
-                    + " - "
-                    + e.getCause().getMessage());
-          } else {
-            throw e;
-          }
-        }
-      }
-      if (receiveMessageResult == null) {
-        throw new RuntimeException("Final fail", receiveException);
-      }
+      ReceiveMessageResponse receiveMessageResult =
+          sqsClient.receiveMessage(
+              ReceiveMessageRequest.builder()
+                  .queueUrl(queueUrl.toString())
+                  .maxNumberOfMessages(10)
+                  .waitTimeSeconds(20)
+                  .build());
 
       List<RecieveRequest<T>> result = new ArrayList<>();
-      for (Message message : receiveMessageResult.getMessages()) {
+      for (Message message : receiveMessageResult.messages()) {
         result.add(
-            new RecieveRequest<>(message, Utils.jsonMapper.readValue(message.getBody(), clazz)));
+            new RecieveRequest<T>(message, Utils.jsonMapper.readValue(message.body(), clazz)));
       }
       return result;
+
     } catch (Exception e) {
       throw new RuntimeException("Failed to process for queue " + queueUrl, e);
     }
@@ -233,21 +216,26 @@ public class SqsManager implements Closeable {
 
   public void deleteQueueMessage(URI queueUrl, List<RecieveRequest<ScraperUpload>> message) {
     sqsClient.deleteMessageBatch(
-        queueUrl.toString(),
-        message.stream()
-            .map(RecieveRequest::message)
-            // TODO: Need to force unique id's because getMessageId is duplicated?
-            .map(
-                e ->
-                    new DeleteMessageBatchRequestEntry(
-                        UUID.randomUUID().toString(), e.getReceiptHandle()))
-            .collect(Collectors.toList()));
+        DeleteMessageBatchRequest.builder()
+            .queueUrl(queueUrl.toString())
+            .entries(
+                message.stream()
+                    .map(RecieveRequest::message)
+                    .map(
+                        e ->
+                            DeleteMessageBatchRequestEntry.builder()
+                                // TODO: Need to force unique id's because getMessageId is duplicated?
+                                .id(UUID.randomUUID().toString())
+                                .receiptHandle(e.receiptHandle())
+                                .build())
+                    .collect(Collectors.toList()))
+            .build());
   }
 
   @Override
   public void close() {
     log.info("closing SqsManager");
-    sqsClient.shutdown();
+    sqsClient.close();
   }
 
   public static String getQueueName(URI queueUrl) {
@@ -272,21 +260,21 @@ public class SqsManager implements Closeable {
     return str.replace("_", ".");
   }
 
-  /** Extends PredefinedRetryPolicies.DEFAULT_RETRY_CONDITION */
-  public static class MyRetryCondition extends SDKDefaultRetryCondition {
-
-    @Override
-    public boolean shouldRetry(
-        AmazonWebServiceRequest originalRequest,
-        AmazonClientException exception,
-        int retriesAttempted) {
-      boolean result = super.shouldRetry(originalRequest, exception, retriesAttempted);
-      log.warn(
-          "Retry {} continue {} due to exception {}",
-          retriesAttempted,
-          result,
-          ExceptionUtils.getRootCauseMessage(exception));
-      return result;
-    }
-  }
+//  /** Extends PredefinedRetryPolicies.DEFAULT_RETRY_CONDITION */
+//  public static class MyRetryCondition extends SDKDefaultRetryCondition {
+//
+//    @Override
+//    public boolean shouldRetry(
+//        AmazonWebServiceRequest originalRequest,
+//        AmazonClientException exception,
+//        int retriesAttempted) {
+//      boolean result = super.shouldRetry(originalRequest, exception, retriesAttempted);
+//      log.warn(
+//          "Retry {} continue {} due to exception {}",
+//          retriesAttempted,
+//          result,
+//          ExceptionUtils.getRootCauseMessage(exception));
+//      return result;
+//    }
+//  }
 }
